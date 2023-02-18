1ff198c478a9cf76c9066b53c6d526e54bdad061
joykara <joykarani1@gmail.com>
2023-02-18 15:37:12 +0300

    create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+blank_board = """
+    1    2   3
+1      |   |  
+    ------------
+2      |   |  
+    --- --- ---
+3      |   |  
+"""
+
+# start your code here
+name = input("What is your name? ")
+
+print("Welcome to Tic Tac Toe, "+name+". Here is our playing board:")
+
+print(blank_board)
+
+position = input("Enter a position (i.e., 1,1): ")
+print(position)
